af109d0dd6a540d5b562df12a13fd4064b52fa0f
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2021-02-21 02:21:54 +0200

    Add menu

--- PiBot/PiBot/PiBot.cpp
+++ PiBot/PiBot/PiBot.cpp
@@ -20,7 +20,40 @@ void title()
     cout << "\\/_/            \\/_________/      \\/_____________/  \\/_________/     \\_\\/          " << endl;
 }
 
+void menu() 
+{
+    cout << setw(56) << endl << "----------------------------" << endl;
+    cout << setw(47) << endl << "      Menu   " << endl;
+    cout << setw(56) << endl << "----------------------------" << endl;
+
+    bool check = false;
+
+    while (check != true)
+    {
+        int input;
+        cout << "\n" << endl;
+        cout << setw(49) << endl << "Enter choice:";
+        cin >> input;
+        cout << "\n" << endl;
+
+        switch (input)
+        {
+        case 1:
+            cout << "Still in progress" << endl;
+            break;
+        case 2:
+            cout << "Still in progress" << endl;
+            break;
+        case 0:
+            check = true;
+        default:
+            break;
+        }
+    }
+}
+
 int main()
 {
     title();
+    menu();
 }
